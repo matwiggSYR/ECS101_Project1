acb482fb7f8a5b74332da7a301d879bfa19d02b1
matwiggSYR <115006854+matwiggSYR@users.noreply.github.com>
2022-10-11 19:33:41 -0400

    Update Team6-Code.py

--- Team6-Code.py
+++ Team6-Code.py
@@ -1,24 +1,19 @@
 import makeDictionary
-import random
 def Code(txtFile):
     myTxtFile = open(txtFile)
-    randomNum = random.randint(1,5)
     myStr = myTxtFile.read()
-    asciiList = []
+    myKey = makeDictionary.getCodeMap('Char2Bin')
     binaryList = []
     for i in myStr:
-        asciiList.append(ord(i))
-    for i in asciiList:
-        binaryList.append(str(int(bin(i)[2:])))
+        binaryList.append(myKey.get(i))
     binaryStr = ''.join(binaryList)
     myDec = 0
-    myKey = makeDictionary.getCodeMap('Char2Bin')
     for i in myStr:
         temp = str(myKey.get(i))
-        print("temp:",temp)
         myDec = str(myDec)
         myDec = bin(int(myDec,2) + int(temp,2))[2:]
     myDec = int(myDec,2)
+    myDec = round(myDec/10)
     myDecStr = str(myDec)
     myOutput = myDecStr+"."+binaryStr
     out_file = open('bin_output.txt','w+')
